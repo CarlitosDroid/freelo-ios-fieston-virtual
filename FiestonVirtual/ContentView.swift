0bf009ef28e22f8f4ed00a185cefbb03a161c3cd
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-09-06 11:33:23 -0500

    Initial Commit

---
+++ FiestonVirtual/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FiestonVirtual
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 9/6/20.
+//  Copyright © 2020 Spydevs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
