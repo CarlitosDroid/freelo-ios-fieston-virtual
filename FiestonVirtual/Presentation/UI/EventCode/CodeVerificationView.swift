1720553e9b350f516713f3b1d0531f477ef1743b
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-09-06 21:36:49 -0500

    Working on DataBase

--- FiestonVirtual/Presentation/UI/EventCode/CodeVerificationView.swift
+++ FiestonVirtual/Presentation/UI/EventCode/CodeVerificationView.swift
@@ -10,28 +10,68 @@ import SwiftUI
 
 struct CodeVerificationView: View {
     
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(
+        entity: UserEntity.entity(),
+        sortDescriptors: [
+            NSSortDescriptor(keyPath: \UserEntity.name, ascending: true)
+        ]
+    ) var userEntities: FetchedResults<UserEntity>
+    
     @ObservedObject var viewModel: CodeVerificationViewModel
+    
+    @State private var name: String = "Tim"
     
     var body: some View {
         VStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            TextField("Enter your name", text: $name)
             Button(action: {
                 self.viewModel.verifyCode()
             }) {
                 Text("Entrar")
             }
+            Button(action: {
+                print(self.userEntities[0])
+            }) {
+                Text("Mostrar")
+            }
+        }
+    }
+    
+    func addUser() {
+        let userEntity = UserEntity(context: managedObjectContext)
+        userEntity.name = "Carlos"
+        userEntity.age = "87"
+        saveContext()
+    }
+    
+    func deleteUser() {
+        
+    }
+   
+    func saveContext() {
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("Error saving managed object context \(error)")
         }
     }
 }
 
 struct CodeVerificationView_Previews: PreviewProvider {
     static var previews: some View {
-        CodeVerificationView(viewModel: CodeVerificationViewModel(
-            loginUseCase: LoginUseCaseImpl(
-            eventCodeRepository: EventCodeRepositoryImpl(
-                eventCodeRemoteDataSource: EventCodeRemoteDataSourceImpl(
-                    eventCodeApi: EventCodeApiImpl())),
-            usersRepository:
-                UsersRepositoryImpl())))
+        CodeVerificationView(
+            viewModel: CodeVerificationViewModel(
+                loginUseCase: LoginUseCaseImpl(
+                    eventCodeRepository: EventCodeRepositoryImpl(
+                        eventCodeRemoteDataSource: EventCodeRemoteDataSourceImpl(
+                            eventCodeApi: EventCodeApiImpl())
+                    ),
+                    usersRepository: UsersRepositoryImpl()
+                )
+            )
+        )
     }
 }
