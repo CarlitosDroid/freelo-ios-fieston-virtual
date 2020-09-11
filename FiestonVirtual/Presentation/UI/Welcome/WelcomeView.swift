d859c86f018fdaadc231bcc9babc2d94a60b0543
Bill Flores <bflorech@everis.com>
2020-09-11 00:26:25 -0500

    update welcome view

--- FiestonVirtual/Presentation/UI/Welcome/WelcomeView.swift
+++ FiestonVirtual/Presentation/UI/Welcome/WelcomeView.swift
@@ -10,8 +10,8 @@ struct WelcomeView: View {
     var body: some View {
         VStack {
             EmptyView()
-        } .sheet(isPresented: $showSheet) {
-            VStack(alignment: .center, spacing: 20) {
+        }.sheet(isPresented: $showSheet) {
+            VStack(alignment: .center, spacing: 10) {
                 
                 HStack {
                     Spacer()
@@ -19,12 +19,10 @@ struct WelcomeView: View {
                         self.showSheet.toggle()
                     }){
                         Text("X").font(.title).bold().foregroundColor(Color(.white))
-                    }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
-                }
+                        }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+                }.hidden()
                 
-                //Image("Fieston").resizable().frame(width: 150, height:50)
-           
-                Image("Fieston")
+                Image("Fieston").resizable().frame(width: 200, height:60)
                 
                 Text(self.welcome.title).font(.title).bold().foregroundColor(Color.amber_600).multilineTextAlignment(.center).fixedSize(horizontal: false, vertical: true)
                 
@@ -41,16 +39,17 @@ struct WelcomeView: View {
                 
                 Spacer()
                 
-                
             }.padding(20).background(Color.deep_purple_intense)
             
         }
     }
 }
-/*
- struct WelcomeView_Previews: PreviewProvider {
- static var previews: some View {
- WelcomeView(welcome: Welcome())
- }
- }
- */
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView(
+            welcome: Welcome(title: "", description: "", subtitle: "", imageUrl: ""),
+            showSheet: true
+        )
+    }
+}
