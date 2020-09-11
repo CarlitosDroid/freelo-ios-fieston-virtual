ac93c7a764b4fb20664165570d449c94b6b81f70
Bill Flores <bflorech@everis.com>
2020-09-10 21:52:04 -0500

    save changes to WelcomeView

--- FiestonVirtual/Presentation/UI/Main/MainView.swift
+++ FiestonVirtual/Presentation/UI/Main/MainView.swift
@@ -12,32 +12,42 @@ struct MainView: View {
     
     @ObservedObject var viewModel = DependencyProvider().assembler.resolver.resolve(WelcomeViewModel.self)!
     @State var isPresented=false
+    @State private var showSheet = false
     
     var body: some View {
-        TabView {
-            Text("HOLA1")
-                .tabItem {
-                    Image(systemName: "list.dash")
-                    Text("Menu")
+        ZStack {
+            Color.deep_purple_intense.edgesIgnoringSafeArea(.all)
+            LoadingView(isShowing: .constant(viewModel.isLoading)) {
+                TabView {
+                    Text("HOLA1")
+                        .tabItem {
+                            Image(systemName: "list.dash")
+                            Text("Menu")
+                    }
+                    PhotosView()
+                        .tabItem {
+                            Image(systemName: "list.dash")
+                            Text("Menu")
+                    }
+                }.navigationBarBackButtonHidden(true)
+                    .sheet(isPresented: self.$viewModel.isSuccessCode ,content:{
+                        WelcomeView(
+                            welcome:self.viewModel.welcome,
+                            showView: self.$viewModel.isSuccessCode
+                        )
+                    }).onAppear{
+                        self.viewModel.getWelcome()
+                        
+                }
+                
             }
-            PhotosView()
-                .tabItem {
-                    Image(systemName: "list.dash")
-                    Text("Menu")
-            }
-        }.navigationBarBackButtonHidden(true)
-            .sheet(isPresented: self.$viewModel.isSuccessCode,content:{
-                WelcomeView(
-                    welcome:self.viewModel.welcome
-                )
-            }).onAppear{
-                self.viewModel.getWelcome()
         }
-    }
 }
-
-struct MainView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainView()
-    }
+/*
+ struct MainView_Previews: PreviewProvider {
+ static var previews: some View {
+ //MainView()
+ }
+ }
+ */
 }
--- FiestonVirtual/Presentation/UI/Welcome/WelcomeView.swift
+++ FiestonVirtual/Presentation/UI/Welcome/WelcomeView.swift
@@ -1,18 +1,49 @@
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct WelcomeView: View {
     
     var welcome: Welcome
+    @Binding var showView : Bool
     
     var body: some View {
-        VStack{
-            Text(welcome.title).bold().fixedSize(horizontal: true, vertical:true)
-            Text(welcome.description).fixedSize(horizontal: true, vertical:true)
-            Text(welcome.subtitle).fixedSize(horizontal: true, vertical:true)
-            Text(welcome.imageUrl).bold()
+        
+        VStack(alignment: .center, spacing: 20) {
             
-        }
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.showView.toggle()
+                }){
+                    Text("X").font(.title).bold().foregroundColor(Color(.white))
+                }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 10))
+            }
+            
+            Image("Fieston")
+            
+            Text("!Bienvenido a la fiesta de Luis!").font(.title).bold().foregroundColor(Color(.yellow)).multilineTextAlignment(.center).fixedSize(horizontal: false, vertical: true)
+            
+            Text( "Bienvenido a la celebracion por el cumpleaños de Luis Ubillus").font(.body).bold().foregroundColor(Color(.green)).multilineTextAlignment(.center).fixedSize(horizontal: false, vertical: true)
+            
+            Text("No olvides que puede subir tus fotos y jugar trivias para hacer la celebracion más interactiva. Divierte!").font(.body).bold().foregroundColor(Color(.green)).multilineTextAlignment(.center).fixedSize(horizontal: false, vertical: true)
+            
+            Text("Acumula puntos y te ganas un vox sorpresa").font(.body).bold().foregroundColor(Color(.yellow)).multilineTextAlignment(.center).fixedSize(horizontal: false, vertical: true)
+            
+            KFImage(URL(string: "https://img.itdg.com.br/tdg/images/blog/uploads/2020/05/shutterstock_1710468256.jpg")!)
+                .resizable()
+                .renderingMode(.original)
+                .shadow(radius: 0)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+            
+            
+            Spacer()
+            
+            
+            
+        }.padding(20).background(Color(.purple))
+        
     }
     
 }
