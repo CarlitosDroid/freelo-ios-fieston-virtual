82093715e3bbb9d385980563a879a9620f149842
Bill Flores <bflorech@everis.com>
2020-09-18 21:30:37 -0500

    update LoadingView

--- FiestonVirtual/Presentation/Utilities/CommonViews/LoadingView.swift
+++ FiestonVirtual/Presentation/Utilities/CommonViews/LoadingView.swift
@@ -13,27 +13,22 @@ fileprivate let loadingText = "Loading"
 struct LoadingView<Content>: View where Content: View {
     @Binding var isShowing: Bool
     var content: () -> Content
-
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .center) {
-                if (!self.isShowing) {
-                    self.content()
-                } else {
-                    self.content()
-                        .disabled(self.isShowing)
-                        .blur(radius: self.isShowing ? 3 : 0)
-
-                    VStack {
-                        Text("Loading ...")
-                        LoadingIndicator(style: .large)
-                    }
-                    .frame(width: geometry.size.width / 3.0, height: geometry.size.height / 6.0)
-                    .background(Color.secondary.colorInvert())
-                    .foregroundColor(Color.primary)
-                    .cornerRadius(20)
-                    .opacity(self.isShowing ? 1 : 0)
+                self.content()
+                    .disabled(self.isShowing)
+                    .blur(radius: self.isShowing ? 3 : 0)
+                VStack {
+                    Text("Loading ...")
+                    LoadingIndicator(style: .large)
                 }
+                .frame(width: geometry.size.width / 3.0, height: geometry.size.height / 6.0)
+                .background(Color.secondary.colorInvert())
+                .foregroundColor(Color.primary)
+                .cornerRadius(20)
+                .opacity(self.isShowing ? 1 : 0)
             }
         }
     }
