629d5e3ede84d12da9c84c9fd34aa4eb5a3b2fee
Bill Flores <bflorech@everis.com>
2020-09-24 21:45:17 -0500

    add loading to GalleryView

--- FiestonVirtual/Presentation/UI/Main/Gallery/GalleryView.swift
+++ FiestonVirtual/Presentation/UI/Main/Gallery/GalleryView.swift
@@ -9,27 +9,30 @@ struct GalleryView: View {
     var onGalleryItemSelected: (_ galleryItem: GalleryItem) -> Void
     
     var body: some View {
+        
         ZStack{
             
             Color.deep_purple_intense.edgesIgnoringSafeArea(.all)
             
-            ScrollView {
-                
-                Grid(viewModel.galleryItems) { galleryItem in
-                    GalleryItemView(galleryItem: galleryItem){
-                        (galleryItem:GalleryItem) in
-                        onGalleryItemSelected(galleryItem)
-                    }
-                }.gridStyle(
-                    ModularGridStyle(columns: 3, rows: .fixed(130))
-                )
-                
+            LoadingView(isShowing: self.$viewModel.isLoading){
+                ScrollView(.vertical, showsIndicators: true) {
+                    
+                    Grid(viewModel.galleryItems) { galleryItem in
+                        GalleryItemView(galleryItem: galleryItem){
+                            (galleryItem:GalleryItem) in
+                            onGalleryItemSelected(galleryItem)
+                        }
+                    }.gridStyle(
+                        ModularGridStyle(columns: 3, rows: .fixed(130))
+                    )
+                    
+                }
+                .padding(8)
             }
-            .padding(8)
+            
         }
         .onAppear {
             viewModel.getGallery()
-            
         }
         
     }
@@ -39,7 +42,7 @@ struct GalleryView: View {
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView(){
-            (galleryItem: GalleryItem) in 
+            (galleryItem: GalleryItem) in
             
         }
     }
