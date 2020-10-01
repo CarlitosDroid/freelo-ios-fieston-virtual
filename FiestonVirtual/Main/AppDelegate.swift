32e39f04acb1b0f10e58ca7d868ce65bcc238623
Bill Flores <bflorech@everis.com>
2020-09-30 21:27:02 -0500

    add IQKeyboardManagerSwift to AppDelegate

--- FiestonVirtual/Main/AppDelegate.swift
+++ FiestonVirtual/Main/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true
         
         // MARK: - Firebase Configuration
         FirebaseApp.configure()
