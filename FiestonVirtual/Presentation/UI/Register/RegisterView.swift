203826112fa8490b062ed6796a929fedc81377ed
Carlos Leonardo Camilo <carlosleonardovargashuaman@gmail.com>
2021-03-01 22:00:03 -0500

    Agregando pantalla de registro

---
+++ FiestonVirtual/Presentation/UI/Register/RegisterView.swift
@@ -0,0 +1,60 @@
+//
+//  RegisterView.swift
+//  FiestonVirtual
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 1/03/21.
+//  Copyright © 2021 Spydevs. All rights reserved.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    
+    var viewMode: CodeVerificationViewModel
+    
+    @Binding var showRegister: Bool
+    
+    @State private var email: String = ""
+    @State private var name: String = ""
+    
+    var body: some View {
+        
+        VStack {
+            EmptyView()
+        }.sheet(isPresented: $showRegister, content: {
+            
+            VStack(spacing: 10) {
+                Spacer()
+                
+                Image("Fieston").resizable().frame(width: 230, height:60)
+                
+                TextField("Nombre", text: self.$email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(height: 30)
+                
+                TextField("Constraseña", text: self.$name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .frame(height: 30)
+                
+                Button(action: {
+                    self.viewMode.verifyCode(code:"719299")
+                }) {
+                    Text("Registrar")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50, alignment: .center)
+                        .background(Color.amber_600)
+                        .cornerRadius(8)
+                }
+                Spacer()
+            }
+            .padding(10)
+            .background(Color.deep_purple_intense)
+            
+        })
+        
+    }
+}
